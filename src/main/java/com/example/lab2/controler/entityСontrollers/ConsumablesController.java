de5ff6413e3adc042b21879a45b2a018a088f57c
vadym685 <Dfl1v4466>
2022-04-30 23:00:54 +0300

    add HttpServletRequest on all methods

--- src/main/java/com/example/lab2/controler/entityСontrollers/ConsumablesController.java
+++ src/main/java/com/example/lab2/controler/entityСontrollers/ConsumablesController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +22,7 @@ public class ConsumablesController {
     private ConsumablesRepo consumablesRepository;
 
     @GetMapping("/consumables")
-    public List<Consumables> getAllConsumables() {
+    public List<Consumables> getAllConsumables( HttpServletRequest request) {
         return consumablesRepository.findAll();
     }
 
