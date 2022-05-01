4e9d0febb30ad66e6409b3679d74c444d628987f
vadym685 <Dfl1v4466>
2022-05-01 11:47:08 +0300

    add logger events on rest controller's

--- src/main/java/com/example/lab2/controler/entityСontrollers/PersonController.java
+++ src/main/java/com/example/lab2/controler/entityСontrollers/PersonController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -27,23 +28,33 @@ public class PersonController {
     private PersonRepo personsRepository;
 
     @GetMapping("/persons")
-    public List<Person> getAllTasks() {
+    public List<Person> getAllPersons(HttpServletRequest request) {
+        Principal user = request.getUserPrincipal();
+        LOGGER.info("User with name " + user.getName() + " get all persons");
+
         return personsRepository.findAll();
     }
 
     @GetMapping("/person/{id}")
     public ResponseEntity<Person> getPersonById(@PathVariable(value = "id") Long personId, HttpServletRequest request)
             throws ResourceNotFoundException {
+
+        Principal user = request.getUserPrincipal();
+        LOGGER.info("User with name " + user.getName() + " find person with id " + personId);
+
         Person person = personsRepository.findById(personId)
                 .orElseThrow(() -> new ResourceNotFoundException("Person not found for this id :: " + personId));
         return ResponseEntity.ok().body(person);
     }
 
     @PostMapping("/person")
-    public ResponseEntity<Person> addPerson(@Validated @RequestBody Person request) {
+    public ResponseEntity<Person> addPerson(@Validated @RequestBody Person requestBody, HttpServletRequest request) {
+        Principal user = request.getUserPrincipal();
+        LOGGER.info("User with name " + user.getName() + "add new person");
+        LOGGER.info("Request body:" + requestBody);
 
         try {
-            Person person = personsRepository.save(request);
+            Person person = personsRepository.save(requestBody);
             return new ResponseEntity<>(null, HttpStatus.OK);
         } catch (Exception e) {
             LOGGER.error(Arrays.toString(e.getStackTrace()));
@@ -55,6 +66,10 @@ public class PersonController {
     @PutMapping("/person/{id}")
     public ResponseEntity<Person> updatePerson(@PathVariable(value = "id") Long personId,
                                                @Validated @RequestBody Person personNew, HttpServletRequest request) throws ResourceNotFoundException {
+        Principal user = request.getUserPrincipal();
+        LOGGER.info("User with name " + user.getName() + " update person with id " + personId);
+        LOGGER.info("Request body:" + personNew);
+
         Person person = personsRepository.findById(personId)
                 .orElseThrow(() -> new ResourceNotFoundException("Person not found for this id :: " + personId));
 
@@ -70,6 +85,9 @@ public class PersonController {
     @DeleteMapping("/person/{id}")
     public Map<String, Boolean> deletePerson(@PathVariable(value = "id") Long personId, HttpServletRequest request)
             throws ResourceNotFoundException {
+        Principal user = request.getUserPrincipal();
+        LOGGER.info("User with name " + user.getName() + " delete person with id " + personId);
+
         Person person = personsRepository.findById(personId)
                 .orElseThrow(() -> new ResourceNotFoundException("Person not found for this id :: " + personId));
 
